67523999239628132bf57f8baf3504c0d44e4046
= <=>
2019-08-26 02:12:34 +0700

    a

--- db.php
+++ db.php
@@ -1,4 +1,6 @@
 <?php
+require_once './login.php';
+
 $connection = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 
     dbname=d4o35efmirk4nt user=zgyigwrhrgyskg 
     password=2475c60d24fecc07282a09a3d8aa743635d429ca69f0eb0bd621cbeba006b00d");  
--- login.php
+++ login.php
@@ -6,7 +6,6 @@
 <body>
 <h1 align="center"> Login </h1>
 <a align="center">
-	<form action="process.php" method="Post"> 
 	User name: <input type="text" name="user"> <br> <br>
 	Password: <input type="password" name="pass"><br> <br>
 	<input type="Submit" valua="Login">
