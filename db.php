a8ab2571dff026d54e7bc9a5a644445e527d1929
= <=>
2019-08-26 10:41:48 +0700

    aaa

--- db.php
+++ db.php
@@ -1,12 +1,34 @@
-<?php
-require_once 'index1.php'; ?>
-
-<?php
-
-$connection = pg_connect("host=ec2-107-20-155-148.compute-1.amazonaws.com port=5432 
-    dbname=d4o35efmirk4nt user=zgyigwrhrgyskg 
-    password=2475c60d24fecc07282a09a3d8aa743635d429ca69f0eb0bd621cbeba006b00d");  
- if(!$connection) {
-     die("Database connection failed");
- }
- ?>
+<?php 
+$hostname= 'ec2-184-73-169-163.compute-1.amazonaws.com';
+// username and password is the account of phpmyadmin
+$username= 'jbkbbnprzowmds';
+$password= 'c6dce3cbd5e39b2922e5a631f020f7843bb8ecf50a4232d14de6c6f1c249652c';
+$dbname= 'd79pkm4gfki68u';
+$port=5432;
+function query($sql)
+{
+    global $hostname; // hàm truyền vào câu truy vấn mình muốn chạy 
+    global $username;
+    global $password;
+    global $dbname;
+    global $port;
+$conn = new mysqli($hostname, $username, $password, $dbname, $port);
+	if($conn-> connect_error)
+   {
+	// nếu như kết nối không thành công thì dừng chương trình 
+	echo "Connection fail";
+	//dừng chương trình
+	die($conn-> connect_error);
+   }
+// chạy câu truy vấn lấy kết quả 
+$result = $conn -> query($sql);
+if(!$result)
+   {
+  //nếu không có kết quả ( $result = null ) thì dừng chương trình 
+  echo "SQL excution fail";
+  die ($conn -> error);
+   }
+// lấy tất cả các bản ghi từ kết quả 
+$rows = mysqli_fetch_all($result);
+return $rows;
+}
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+<h1 align="center"> Login </h1>
+<a align="center">
+	<form action="process.php" method="Post"> 
+	User name: <input type="text" name="user"> <br> <br>
+	Password: <input type="password" name="pass"><br> <br>
+	<input type="Submit" valua="Login">
+	<input type="Reset" valua="Reset">
+    </form>
+</a>
+</body>
+</html>
